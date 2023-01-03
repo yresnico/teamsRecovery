f3aba084e095885a3799d996668fa029b397e5bb
Reuven Resnicoff <yresnicoff@gmail.com>
2023-01-03 13:29:05 +0200

    Initial commit for orders

--- client/client-app/src/App.js
+++ client/client-app/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Orders from './components/Orders';
 import SignUp from './components/SignUp';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/orders' element={<Orders />} />
         </Routes>
       </Router>
     </div>
